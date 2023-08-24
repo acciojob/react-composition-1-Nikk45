3e8a09d335574a4cd31d33ec9826ac468a5329ff
Nikk45 <nicksin36@gmail.com>
2023-08-24 14:06:04 +0530

    submit 1 7

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ const App = () => {
         {/* Do not remove the main div */}
 
         <Tabs tabs={tabs} setResult={setResult}/>
-        {result && <p>{result}</p>}
+        {result && result}
     </div>
   )
 }
